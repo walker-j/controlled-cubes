e44f70a1027e0c57a1abe512ad3bb9fbe2d81091
walker-j <james.walker150@gmail.com>
2018-12-17 11:49:43 +0000

    Removed extra toolchain generated bits, serviceWorker, logo

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { Button, Slider } from "@blueprintjs/core";
-import { ChromePicker } from 'react-color';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button>Test button</button>
-          <Button 
-            intent="primary" 
-            text="button content"
-            onClick={alert}
-          />
-          <Slider
-                    min={0}
-                    max={10}
-                    stepSize={0.1}
-                    labelStepSize={10}
-                />
-          {/* <ChromePicker /> */}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		const content = "App contents"
+
+		return (
+			<div className="App">
+				{content}
+			</div>
+		);
+	}
 }
 
 export default App;
